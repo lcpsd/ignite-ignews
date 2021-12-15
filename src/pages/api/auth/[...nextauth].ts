e8a60fe3ef34f2888c9133c1a185fcb033330900
Lucas Cardoso <lucascardosopsd@gmail.com>
2021-12-15 19:12:08 -0300

    github-login

--- src/pages/api/auth/[...nextauth].ts
+++ src/pages/api/auth/[...nextauth].ts
@@ -1,6 +1,8 @@
 import NextAuth from 'next-auth'
+import { signIn } from 'next-auth/client'
 import Providers from 'next-auth/providers'
-
+import {fauna} from '../../../services/fauna'
+import { query as q } from 'faunadb'
 
 export default NextAuth({
     providers:[
@@ -11,4 +13,16 @@ export default NextAuth({
         })
     ],
 
+    callbacks:{
+        async signIn(user, account, profile){
+            const {email} = user
+            await fauna.query(
+                q.Create(
+                    q.Collection('users'), 
+                    { data: { email } }
+                )
+            )
+            return true
+        }
+    }
 })
