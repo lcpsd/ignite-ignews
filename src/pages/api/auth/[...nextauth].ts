19cd78e8302368bd10f63d7febd9cb58f325bd0e
Lucas Cardoso <lucascardosopsd@gmail.com>
2021-12-17 11:37:24 -0300

    jwt secret removed

--- src/pages/api/auth/[...nextauth].ts
+++ src/pages/api/auth/[...nextauth].ts
@@ -4,8 +4,6 @@ import {fauna} from '../../../services/fauna'
 import { query as q } from 'faunadb'
 
 export default NextAuth({
-    jwt: {signingKey: process.env.JWT_SECRET},
-
     providers:[
         Providers.GitHub({
             clientId: process.env.GITHUB_CLIENT_ID,
@@ -19,13 +17,28 @@ export default NextAuth({
             const {email} = user
             try {
                 await fauna.query(
-                    q.Create(
-                        q.Collection('users'), 
-                        { data: { email } }
+                    q.If(
+                        q.Not(
+                            q.Exists(
+                                q.Match(
+                                    q.Index('user_by_email'), q.Casefold(user.email)
+                                )
+                            )
+                        ),
+                        q.Create(
+                            q.Collection('users'), 
+                            { data: { email } }
+                        ),
+                        q.Get(
+                            q.Match(
+                                q.Index('user_by_email'), q.Casefold(user.email)
+                            )
+                        )
                     )
                 )
                 return true
             } catch (error) {
+                console.log(error)
                 return false
             }
         }
